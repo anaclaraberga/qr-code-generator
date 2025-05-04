464619693bc01a162d01359c9a40f2edc48f8f03
Ana Clara <ana4ew.br@gmail.com>
2025-05-03 23:51:51 -0300

    Created: Main Controller

---
+++ src/main/java/com/br/qr_code_generator/controller/QrCodeController.java
@@ -0,0 +1,21 @@
+package com.br.qr_code_generator.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.qr_code_generator.dtos.QrCodeGeneratorRequest;
+import com.br.qr_code_generator.dtos.QrCodeGeneratorResponse;
+
+@RestController
+@RequestMapping("/qrcode")
+public class QrCodeController {
+
+    @PostMapping
+    public ResponseEntity<QrCodeGeneratorResponse> generate(@RequestBody QrCodeGeneratorRequest request) {
+        return null;
+    }
+    
+}
